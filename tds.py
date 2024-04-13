558b670b063ed2a3e49e359e92d594fa985dc021
[PatoTicoQuack] <mrogeriopesarini@gmail.com>
2024-04-12 23:51:41 -0300

    Atualizar exibição do gráfico

--- tds.py
+++ tds.py
@@ -6,25 +6,28 @@ import seaborn as sns
 # Lendo o arquivo
 dados = pd.read_excel("acidentes.xlsx")
 
-# Separando as informações por ';'
-dados = dados.iloc[:, 0].str.split(';', expand=True)
+# Criando uma figura com quatro subplots
+fig, axs = plt.subplots(4, 1, figsize=(12,24))
 
-# Selecionando a primeira coluna
-acidentes = dados.iloc[:, 0]
+# Para cada coluna
+for i in range(4):
+    # Selecionando a coluna
+    coluna = dados.iloc[:, i]
 
-# Contando a frequência de cada tipo de acidente
-frequencia_acidentes = acidentes.value_counts()
+    # Contando a frequência de cada valor
+    frequencia = coluna.value_counts()
 
-# Filtrando os acidentes com frequência maior que 1000
-frequencia_acidentes_filtrada = frequencia_acidentes[frequencia_acidentes > 1000]
+    # Se a coluna é a primeira ou a segunda
+    if i < 2:
+        # Filtrando os valores com frequência maior que 1000
+        frequencia = frequencia[frequencia > 2000]
 
-# Plotando o gráfico
-fig = plt.figure(figsize=(12,6))
-sns.barplot(x=frequencia_acidentes_filtrada.index, y=frequencia_acidentes_filtrada.values)
-plt.title('Acidentes Mais Ocorridos')
-plt.xlabel('Tipo de Acidente')
-plt.ylabel('Frequência')
-plt.xticks(rotation=90)
+    # Plotando o gráfico
+    sns.barplot(x=frequencia.index, y=frequencia.values, ax=axs[i])
+    axs[i].set_title('Coluna ' + str(i+1))
+    axs[i].set_xlabel('Valor')
+    axs[i].set_ylabel('Frequência')
+    plt.xticks(rotation=90)
 
 # Ajustando o espaço no fundo do gráfico
 plt.subplots_adjust(bottom=0.25)
